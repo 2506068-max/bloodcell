4495412a774754e0b3d9165872a1c13cfec58327
2506068-max <2506068@itg.ac.id>
2026-05-31 23:19:34 +0700

    fix vite config conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-})
->>>>>>> 9240bb951cf8f659ca3ab8691a0d2d23505313de
